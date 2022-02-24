0013b3051198855818f6050351ca1a9057de5ae9
EC2 Default User <ec2-user@ip-172-31-77-123.ec2.internal>
2022-02-24 03:39:20 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+        self.select(:rating).map(&:rating).uniq
+    end
+    
+    def self.with_ratings(ratings_list, header: nil)
+        ratings = ratings_list.present? ? ratings_list.keys : all_ratings
+        ratings = ratings.map { |rating| rating.upcase }
+        return self.where(rating: ratings) unless header.present?
+        return self.where(rating: ratings).order(header) if header.present?
+    end
 end
